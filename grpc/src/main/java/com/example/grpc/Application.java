a902e152df1e69a47e27a4a0ee5a29a2abd568c7
haifeng.pang <panghaifeng@weidian.com>
2020-06-30 18:32:01 +0800

    grpc

---
+++ grpc/src/main/java/com/example/grpc/Application.java
@@ -0,0 +1,8 @@
+package com.example.grpc;
+
+/**
+ * @author haifeng.pang [panghaifeng@weidian.com]
+ * @version 1.0 : Application v0.1 2020/6/30 18:31 haifeng.pang Exp $
+ **/
+public class Application {
+}
